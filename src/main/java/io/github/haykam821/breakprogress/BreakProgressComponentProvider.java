30870767265e6353461bec3ff6d3ef6a5b304e87
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-06-14 10:15:57 -0400

    Update mod to Minecraft 1.19

--- src/main/java/io/github/haykam821/breakprogress/BreakProgressComponentProvider.java
+++ src/main/java/io/github/haykam821/breakprogress/BreakProgressComponentProvider.java
@@ -1,24 +1,21 @@
 package io.github.haykam821.breakprogress;
 
-import java.util.List;
-
 import io.github.haykam821.breakprogress.mixin.ClientPlayerInteractionManagerMixin;
-import mcp.mobius.waila.api.IComponentProvider;
-import mcp.mobius.waila.api.IDataAccessor;
+import mcp.mobius.waila.api.IBlockAccessor;
+import mcp.mobius.waila.api.IBlockComponentProvider;
 import mcp.mobius.waila.api.IPluginConfig;
+import mcp.mobius.waila.api.ITooltip;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 
-@SuppressWarnings("deprecation")
-public class BreakProgressComponentProvider implements IComponentProvider {
+public class BreakProgressComponentProvider implements IBlockComponentProvider {
 	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
 
 	@Override
-	public void appendTail(List<Text> tooltip, IDataAccessor accessor, IPluginConfig config) {
+	public void appendTail(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
 		int progress = (int) (((ClientPlayerInteractionManagerMixin) CLIENT.interactionManager).getCurrentBreakingProgress() * 100);
 		if (progress > 0) {
-			tooltip.add(new TranslatableText("text.breakprogress.tooltip", progress));
+			tooltip.addLine(Text.translatable("text.breakprogress.tooltip", progress));
 		}
 	}
 }
--- src/main/java/io/github/haykam821/breakprogress/BreakProgressWailaPlugin.java
+++ src/main/java/io/github/haykam821/breakprogress/BreakProgressWailaPlugin.java
@@ -5,10 +5,9 @@ import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.TooltipPosition;
 import net.minecraft.block.Block;
 
-@SuppressWarnings("deprecation")
 public class BreakProgressWailaPlugin implements IWailaPlugin {
 	@Override
     public void register(IRegistrar registrar) {
-		registrar.registerComponentProvider(new BreakProgressComponentProvider(), TooltipPosition.TAIL, Block.class);
+		registrar.addComponent(new BreakProgressComponentProvider(), TooltipPosition.TAIL, Block.class);
 	}
 }
